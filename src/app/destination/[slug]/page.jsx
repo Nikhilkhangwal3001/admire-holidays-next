7126660f6b516cb3dc1594a17cb024560f3b780e
Ravi24052003 <ravibusinessac291@gmail.com>
2025-04-16 12:07:56 +0530

    solved module import error

--- src/app/destination/[slug]/page.jsx
+++ src/app/destination/[slug]/page.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Itinary from "@/app/itinary";
+// import Itinary from "@/app/itinerary";
 import Image from "next/image";
 import Gallery from "@/app/detailpage.jsx/gallery";
 import axios from "axios";
